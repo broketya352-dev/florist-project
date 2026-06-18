78112b8d4f68a177d3525adfa7b3d52122e21d06
Kiri100zz <kirita11024@gmail.com>
2026-06-18 22:02:58 +0700

    costumer.tracker

--- customer.c
+++ customer.c
@@ -1,4 +1,52 @@
 #include <stdio.h>
+#include <string.h>
 #include "customer.h"
 
-/* function implementations go here */
+void addCustomer(Customer customers[], int *count) {
+    printf("\n--- Add New Customer ---\n");
+    
+    if (*count >= MAX_CUSTOMERS) {
+        printf("Error: Customer database is full (%d/%d).\n", *count, MAX_CUSTOMERS);
+        return;
+    }
+
+    int newID = *count + 1;
+    customers[*count].customerID = newID;
+    printf("Assigned Customer ID: %d\n", newID);
+
+    printf("Enter Customer Name: ");
+    
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF); 
+
+    fgets(customers[*count].name, MAX_NAME, stdin);
+    customers[*count].name[strcspn(customers[*count].name, "\n")] = '\0';
+
+    printf("Enter Phone Number: ");
+    fgets(customers[*count].phone, MAX_PHONE, stdin);
+    customers[*count].phone[strcspn(customers[*count].phone, "\n")] = '\0';
+
+    (*count)++;
+    
+    printf("\nCustomer '%s' added successfully!\n", customers[*count - 1].name);
+}
+
+void displayCustomers(Customer customers[], int count) {
+    printf("\n--- Customer List ---\n");
+    
+    if (count == 0) {
+        printf("No customers registered yet.\n");
+        return;
+    }
+
+    printf("%-12s | %-25s | %-15s\n", "Customer ID", "Name", "Phone Number");
+    printf("------------------------------------------------------------\n");
+
+    for (int i = 0; i < count; i++) {
+        printf("%-12d | %-25s | %-15s\n", 
+               customers[i].customerID, 
+               customers[i].name, 
+               customers[i].phone);
+    }
+    printf("------------------------------------------------------------\n");
+}
