284ab362492bfd89a63a358bf6f7e2fabb739fc9
Kiri100zz <kirita11024@gmail.com>
2026-06-23 11:48:44 +0700

    Add salesReport function for order receipt generation
    Implement salesReport function to generate order receipts.

--- report.c
+++ report.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
 #include "report.h"
+#include "searchFlower.h"
 
-/* function implementations go here */
+void salesReport(Order orders[], int orderCount, Flower flowers[], int flowerCount) {
+    for (int i = 0; i < orderCount; i++) {
+        printf("\n--- Receipt for Order #%d ---\n", orders[i].orderID);
+        
+        for (int j = 0; j < orders[i].itemCount; j++) {
+            int currentFlowerID = orders[i].item[j].flower.flowerID;
+            
+            searchFlowerByID(flowers, flowerCount, currentFlowerID);
+            
+            printf("Quantity Ordered: %d\n", orders[i].item[j].quantity);
+        }
+    }
+}
