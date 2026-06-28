4c3a7579f6e64d847a8dbdbee997116bd38429bf
Kiri100zz <kirita11024@gmail.com>
2026-06-28 21:45:14 +0700

    Update customer.h

--- customer.h
+++ customer.h
@@ -7,6 +7,7 @@ void addCustomer(Customer customers[], int *count);
 void displayCustomers(Customer customers[], int count);
 int searchCustomer(Customer customers[], int count, char *phone);
 void findCustomer(Customer customers[], int count);
+void deleteCustomer(Customer customers[], int *count, char *phone);
 
 
 #endif
