92987c3112f5c1942a05460c93d718a053bbdc7c
Kiri100zz <kirita11024@gmail.com>
2026-06-28 21:18:05 +0700

    Update flower.h

--- flower.h
+++ flower.h
@@ -6,5 +6,6 @@
 void addFlower(Flower flowers[], int *count);
 void displayFlowers(Flower flowers[], int count);
 void updateFlowerStock(Flower flowers[], int count);
+void deleteFlower(Flower flowers[], int *count);
 
 #endif
